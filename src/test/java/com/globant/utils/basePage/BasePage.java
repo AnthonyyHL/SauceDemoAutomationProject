634cdf744f889a12eee90bad8bef7502143623fe
AnthonyyHL <anthleon@espol.edu.ec>
2024-11-11 18:17:59 -0500

    feat: Implement test data provider in Data class

--- src/test/java/com/globant/utils/basePage/BasePage.java
+++ src/test/java/com/globant/utils/basePage/BasePage.java
@@ -2,8 +2,11 @@ package com.globant.utils.basePage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BasePage {
     WebDriver driver;
@@ -11,6 +14,8 @@ public class BasePage {
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(8));
     }
 
     public WebDriver getDriver() {
