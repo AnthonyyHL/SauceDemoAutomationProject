73b57983f592dca240fda40d7868fadf9da58213
AnthonyyHL <anthleon@espol.edu.ec>
2024-12-03 21:15:35 -0500

    feat: Implement capabilities for non-visual browser execution

--- src/test/java/com/globant/utils/DriverManager.java
+++ src/test/java/com/globant/utils/DriverManager.java
@@ -2,22 +2,32 @@ package com.globant.utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import resources.ConfigCapabilities;
 
-public class DriverManager {
+public class DriverManager extends ConfigCapabilities {
     private final WebDriver driver;
 
     public DriverManager(String browserType) {
+        super();
         String browser = browserType.toLowerCase();
         switch (browser) {
             case "chrome":
-                driver = WebDriverManager
-                        .chromedriver()
-                        .create();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless");
+                chromeOptions.merge(super.loadCapabilities());
+                driver = new ChromeDriver(chromeOptions);
                 break;
             case "firefox":
-                driver = WebDriverManager
-                        .firefoxdriver()
-                        .create();
+                FirefoxOptions options = new FirefoxOptions();
+                options.addArguments("--headless");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.merge(super.loadCapabilities());
+                driver = new FirefoxDriver(options);
                 break;
             default:
                 throw new IllegalArgumentException("Browser type not supported: " + browser);
